8c9e40ca0fe86552f583c9d2f53b72ff05ff2c77
Josh-S-Tyrrell <josh.s.tyrrell@gmail.com>
2026-01-21 12:39:28 +0100

    Initialization

---
+++ src/main/java/org/example/testspringbootproject/TestSpringBootProjectApplication.java
@@ -0,0 +1,13 @@
+package org.example.testspringbootproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestSpringBootProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestSpringBootProjectApplication.class, args);
+    }
+
+}
